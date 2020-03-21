8e33f4c27bcf147a3be48f308ca46e17ca601a09
Anshuman Gupta <anshuman844@gmail.com>
2020-03-21 19:47:21 +0530

    Installed package;  shortid.   - It replace default _id with shorter, random and unique string.

--- models/user.js
+++ models/user.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const ShortId = require('shortid');
 const user = new Schema({
   username: {
     type: String,
     required: true,
     maxlength: 20,
     unique: true
+  },
+  _id: {
+    type: String,
+    default: ShortId.generate
   }
 });
 module.exports = mongoose.model('User', user);
