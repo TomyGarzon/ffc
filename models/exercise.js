c0e0fc7b5a2e661c8044e92c1e215ec9abd91e69
Anshuman Gupta <anshuman844@gmail.com>
2020-03-21 20:31:04 +0530

    Minor bug fixed in exercise model.

--- models/exercise.js
+++ models/exercise.js
@@ -3,19 +3,25 @@ const Schema = mongoose.Schema;
 const exercise = new Schema({
   userId: {
     type: String,
-    required: true
+    required: true,
+    ref: 'User',
+    index: true
   },
+  username: String,
   description: {
     type: String,
-    require: true
+    require: true,
+    maxlength: [20, 'description too long']
   },
   duration: {
     type: Number,
-    required: true
+    required: true,
+    min: [1, 'duration too short']
   },
   date: {
-    type: date,
-    required: true
+    type: Date,
+    required: true,
+    default: Date.now
   }
 });
 module.exports = mongoose.model('Exercise', exercise);
