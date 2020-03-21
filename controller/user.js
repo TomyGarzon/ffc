8ad532c735daa66d18effad33e1881cede02ad46
Anshuman Gupta <anshuman844@gmail.com>
2020-03-21 19:45:47 +0530

    Added method for adding new user.

--- controller/user.js
+++ controller/user.js
@@ -1,8 +1,21 @@
 const User = require('../models/user');
 
 const addUser = (username, callback) => {
-  callback({
-    usernameC: username
+  User.findOne({username: username}, (err, result) => {
+    if (err) callback({error: err});
+    if (result) callback({error: "Username already taken."})
+    else {
+      const newUser = new User({
+        username: username
+      });
+      newUser.save((err, user) => {
+        if (err) callback({error: err});
+        callback({
+          username: user.username,
+          _id: user._id
+        });
+      });
+    }
   });
 }
 
