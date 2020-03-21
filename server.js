e97a79bf035e83b7fde88931eb1b150c1cdee3c5
Anshuman Gupta <anshuman844@gmail.com>
2020-03-21 21:41:53 +0530

    Now new exercise can be created.

--- server.js
+++ server.js
@@ -41,6 +41,37 @@ app.post('/api/exercise/new-user', (req, res) => {
   }
 });
 
+app.post('/api/exercise/add', (req, res) => {
+  const date = req.body.date;
+  const userID = (req.body.userId).trim();
+  const duration = parseInt(req.body.duration);
+  const description = (req.body.description).trim();
+  const {addExercise} = require('./controller/exercise');
+
+  const errors = {};
+  if (!(/^\d{4}-\d{2}-\d{2}$/.test(date)) || (new Date(date).toDateString() === "Invalid Date")) {
+    errors.date = "Invalid date.";
+  }
+  if (isNaN(duration) || duration === 0) {
+    errors.duration = "Invalid duration.";
+  }
+  if (userID === "") {
+    errors.userId = "Invalid user id.";
+  }
+  if (description.length === 0) {
+    errors.description = "Description is require.";
+  }
+  else if (description.length > 20) {
+    errors.description = "Description can't be more than 20 characters long.";
+  }
+  if (Object.keys(errors).length !== 0) {
+    res.json({ errors: errors });
+  }
+  addExercise(userID, description, duration, date, (result) => {
+    res.json(result);
+  });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
